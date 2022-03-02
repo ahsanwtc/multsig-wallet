2e8bf2a1be6b39060aeb928eb2bc71e6800bd43e
jsan <ahsan.wtc@gmail.com>
2022-03-02 19:13:01 +0500

    first test

--- test/Wallet.js
+++ test/Wallet.js
@@ -1,11 +1,23 @@
 const Wallet = artifacts.require('Wallet');
+const QUORUM = 2;
 
-contract('Wallet', (accounts) => {
+contract('Wallet', accounts => {
   let wallet;
-  const quorum = 2;
+  
 
   beforeEach(async () => {
-    wallet = await Wallet.new([accounts[0], accounts[1], accounts[2], quorum]);
+    wallet = await Wallet.new([accounts[0], accounts[1], accounts[2]], QUORUM);
     await web3.eth.sendTransaction({ from: accounts[0], to: wallet.address, value: 1000 });
   });
+
+  it('should have correct approvers and quorum', async () => {
+    const approvers = await wallet.getApprovers();
+    const quorum = await wallet.quorum();
+    assert(approvers.length === 3);
+    assert(approvers[0] === accounts[0]);
+    assert(approvers[1] === accounts[1]);
+    assert(approvers[2] === accounts[2]);
+    assert(quorum.toNumber() === QUORUM);
+  });
+
 });
