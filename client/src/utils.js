fe39664956f7f844b7ad5ac8876d82f63c3ade5d
jsan <ahsan.wtc@gmail.com>
2022-03-05 18:17:45 +0500

    added @metamask/detect-provider for better integration

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,23 +1,36 @@
 import Web3 from "web3";
+import detectEthereumProvider from '@metamask/detect-provider';
+
 import Wallet from './contracts/Wallet.json';
 
 const getWeb3 = () => {
-  return new Promise((resolve, reject) => {
-    window.addEventListener('load', async () => {
-      if (window.ethereum) {
+  return new Promise(async (resolve, reject) => {
+    let provider = await detectEthereumProvider();
+    if (provider) {
+      await provider.request({ method: 'eth_requestAccounts' });
+      try {
         const web3 = new Web3(window.ethereum);
-        try {
-          await window.ethereum.enable();
-          resolve(web3);
-        } catch (error) {
-          reject(error);
-        }
-      } else if (window.web3) {
-        resolve(window.web3);
-      } else {
-        reject('Must install Metamask');
+        resolve(web3);
+      } catch (error) {
+        reject(error);
       }
-    });
+    }
+
+    reject('Must install Metamask');
+      // if (window.ethereum) {
+      //   const web3 = new Web3(window.ethereum);
+      //   try {
+      //     await window.ethereum.enable();
+      //     resolve(web3);
+      //   } catch (error) {
+      //     reject(error);
+      //   }
+      // } else if (window.web3) {
+      //   resolve(window.web3);
+      // } else {
+      //   reject('Must install Metamask');
+      // }
+    
   });
 }
 
