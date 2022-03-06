01c1b88f522cada3a8e8a7032c39272edd1c181f
jsan <ahsan.wtc@gmail.com>
2022-03-06 12:48:30 +0500

    adding rinkeby test network

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const provider = require('@truffle/hdwallet-provider');
+
+const secrets = require('./secrets.json');
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -36,6 +40,15 @@ module.exports = {
    */
   contracts_build_directory: path.join(__dirname, 'client/src/contracts'),
   networks: {
+    rinkeby: {
+      provider: () => new provider(
+        secrets.privateKeys,
+        'https://rinkeby.infura.io/v3/51a62968eb694c679c7cd3b749a588b0',
+        0,
+        3
+      ),
+      network_id: 4
+    }
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
